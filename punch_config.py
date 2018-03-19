ba59f3a788b833991f1e567ccf30fd45f707b29a
Daniel <climbit@gmx.de>
2018-03-19 09:48:21 +0100

    Configure punch to tag the version to the commits.

--- punch_config.py
+++ punch_config.py
@@ -11,4 +11,6 @@ VERSION = ['major', 'minor', 'patch']
 VCS = {
     'name': 'git',
     'commit_message': "Version updated from {{ current_version }} to {{ new_version }} with punch.",
+    'annotate_tags': True,
+    'annotation_message': "Version {{ new_version }}"
 }
